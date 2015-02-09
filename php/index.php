db4fd306bcf160ba2ccaebfa0b82019209126d99
Prashant Sinha <prashnts@live.com>
2015-02-09 10:33:13 +0530

    Fixes

--- php/index.php
+++ php/index.php
@@ -4,22 +4,16 @@ namespace DriveForm;
 require 'delegate.php';
 require 'exceptions.php';
 require 'vendor/google-api-php-client/autoload.php';
+require 'vendor/slim_framework/Slim/Slim.php';
 require 'APIProxy/spreadsheet.php';
+\Slim\Slim::registerAutoloader();
 
-$auth = new \DriveForm\Delegate\Service_Auth();
-$state = new \DriveForm\Delegate\State();
+$_AUTH  = new \DriveForm\Delegate\Service_Auth();
+$_STATE = new \DriveForm\Delegate\State();
+$_APP   = new \Slim\Slim();
 
-# Routing Cases
-$URI = preg_split('/[\/\?]/', preg_replace("/[\/]+/", "/", $_SERVER['REQUEST_URI']));
-const BASE = 2;
+$_APP->get('/', function () {
+    echo "Index Route";
+});
 
-switch(strtolower(isset($URI[BASE]) ? $URI[BASE] : False)) {
-    case 'form':
-        //$spreadsheet = new APIProxy\Google\SpreadSheet();
-        //echo file_get_contents("DriveForm Access-30e4e8b93b86.p12");
-        APIProxy\Google\initClient();
-
-        break;
-    default:
-        echo "Serve Index File here.";
-}
+$_APP->run();
