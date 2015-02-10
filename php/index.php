841efd48ce96139e3c33eea9a60dcb63659534b6
Prashant Sinha <prashnts@live.com>
2015-02-10 17:02:46 +0530

    Fixes

--- php/index.php
+++ php/index.php
@@ -14,10 +14,16 @@ $_AUTH   = new \DriveForm\Delegate\Service_Auth();
 $_STATE  = new \DriveForm\Delegate\State();
 $_APP    = new \Slim\Slim();
 
-
+/*
 $_APP->get('/', function () {
     echo "Index Route";
     $a = new \DriveForm\APIProxy\Google\Spreadsheet();
 });
+*/
+
+$_APP->get('/', function () {
+    # Serving the static files.
+    
+})
 
 $_APP->run();
